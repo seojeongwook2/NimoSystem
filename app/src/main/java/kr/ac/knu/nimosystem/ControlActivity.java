87a1dcc2a5e4a11cd9465ca68949a26d072b5ce9
seojeongwook2 <sju7950@naver.com>
2021-01-03 15:57:54 +0900

    210103 일괄처리 전송 기능 구현 완료

--- app/src/main/java/kr/ac/knu/nimosystem/ControlActivity.java
+++ app/src/main/java/kr/ac/knu/nimosystem/ControlActivity.java
@@ -6,10 +6,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import kr.ac.knu.nimosystem.Adapter.PhoneAdapter;
 import kr.ac.knu.nimosystem.Model.PhoneItem;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,6 +29,7 @@ public class ControlActivity extends AppCompatActivity {
     private Button add_button,login_his_button;
     private RecyclerView recyclerView;
     private ArrayList<PhoneItem> list;
+    private Context context;
 
     /*
     로그인 기록 저장하는 것은 해당 기기에 로그인한 기록만 저장
@@ -34,6 +43,7 @@ public class ControlActivity extends AppCompatActivity {
     }
 
     private void init() {
+        context = this;
         login_type = getIntent().getExtras().getString("LOGIN_TYPE");
         all_control_button = findViewById(R.id.all_control_button);
         next_page_button = findViewById(R.id.next_page_button);
@@ -48,12 +58,13 @@ public class ControlActivity extends AppCompatActivity {
             add_button.setVisibility(View.GONE);
         }
 
-
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(ControlActivity.this, RecyclerView.HORIZONTAL, false));
 
+        list.clear();
         list.add(new PhoneItem("01062817950", "서정욱"));
         list.add(new PhoneItem("01037806514", "장진현"));
+
         final PhoneAdapter adapter = new PhoneAdapter(ControlActivity.this, list);
 
         adapter.setSendInterface(new PhoneAdapter.SendInterface() {
@@ -64,7 +75,6 @@ public class ControlActivity extends AppCompatActivity {
         });
 
         recyclerView.setAdapter(adapter);
-
         button_setting();
         Intent passedIntent = getIntent();
         processIntent(passedIntent);
@@ -74,7 +84,33 @@ public class ControlActivity extends AppCompatActivity {
         all_control_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                android.app.AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                View verifiedView = layoutInflater.inflate(R.layout.dialog_all_send, null);
 
+                Button cancel_button = verifiedView.findViewById(R.id.cancel_button);
+                Button send_button = verifiedView.findViewById(R.id.send_button);
+                final EditText send_body = verifiedView.findViewById(R.id.dialog_all_send_body);
+
+                builder.setView(verifiedView);
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                cancel_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                send_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final String send_content = send_body.getText().toString();
+                        sendMessage(send_content);
+                        alertDialog.dismiss();
+                    }
+                });
             }
         });
 
@@ -104,21 +140,7 @@ public class ControlActivity extends AppCompatActivity {
         if(intent != null){
             String string = intent.getStringExtra("string");
             // Toast.makeText(ControlActivity.this, string, Toast.LENGTH_LONG).show();
-
-            list.clear();
-            list.add(new PhoneItem("01062817950", "서정욱"));
-            list.add(new PhoneItem("01037806514", "장진현"));
-
-            final PhoneAdapter adapter = new PhoneAdapter(ControlActivity.this, list);
-
-            adapter.setSendInterface(new PhoneAdapter.SendInterface() {
-                @Override
-                public void onSignal(int position) {
-                    adapter.notifyItemChanged(position, "sms_receive");
-                }
-            });
-
-            recyclerView.setAdapter(adapter);
+            refresh_list();
         }
     }
 
@@ -126,5 +148,59 @@ public class ControlActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         processIntent(intent);
         super.onNewIntent(intent);
+    }
+
+    private void sendMessage(final String body) {
+        try {
+            androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(ControlActivity.this);
+            builder.setTitle("메시지 전송").setMessage("정말 보내시겠습니까?");
+            builder.setPositiveButton("전송", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    for(int k = 0; k < list.size(); k++) {
+                        smsManager.sendTextMessage(list.get(k).getNumber(), null, body, null, null);
+                        Toast.makeText(ControlActivity.this, list.get(k).getName() + " 으로 전송 완료했습니다.", Toast.LENGTH_LONG).show();
+                    }
+                    refresh_list();
+                }
+            });
+
+            builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+
+            androidx.appcompat.app.AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        } catch(Exception e) {
+            Log.e("send message error", e.getMessage());
+            Toast.makeText(ControlActivity.this, "전송 실패, 다시 시도 하세요.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void refresh_list() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                list.clear();
+                list.add(new PhoneItem("01062817950", "서정욱"));
+                list.add(new PhoneItem("01037806514", "장진현"));
+
+                final PhoneAdapter adapter = new PhoneAdapter(ControlActivity.this, list);
+
+                adapter.setSendInterface(new PhoneAdapter.SendInterface() {
+                    @Override
+                    public void onSignal(int position) {
+                        adapter.notifyItemChanged(position, "sms_receive");
+                    }
+                });
+
+                recyclerView.setAdapter(adapter);
+            }
+        }, 1000);
     }
 }
